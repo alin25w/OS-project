25564b15723dec8664f93c1d2dfd5160893265c4
alin25w <alin.dascalu25@student.upt.ro>
2023-04-08 19:44:04 +0300

    main and libraries

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main(int argc, char* argv[]){
+
+
+}
